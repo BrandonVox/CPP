c71bdb0e6cf5ed594bb05f898f61f3271b0fbd82
brandonvox <brandonvox9@gmail.com>
2023-06-05 04:48:20 +0700

    Third Person Map

---
+++ Source/CPP/CPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_API ACPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
