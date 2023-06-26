934f738c6c34de6ca89932fb92fba65675b9f75d
brandonvox <brandonvox9@gmail.com>
2023-06-26 14:56:38 +0700

    AI Controller

---
+++ Source/CPP/Public/Controller/EnemyAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "EnemyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_API AEnemyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
